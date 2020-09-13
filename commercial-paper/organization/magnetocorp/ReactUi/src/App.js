1299e5d2fc2c9a0864deb1089ceb2174c73b1b39
awhuang0930 <awhuang888@hotmail.com>
2020-09-13 12:52:34 +1000

    placeOrder UI done

--- commercial-paper/organization/magnetocorp/ReactUi/src/App.js
+++ commercial-paper/organization/magnetocorp/ReactUi/src/App.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react';
+import './webflow.css';
+import './deasxui.css';
 import './App.css';
 import { ApolloProvider, ApolloClient, InMemoryCache, useQuery, gql, useMutation } from '@apollo/client';
 
@@ -20,6 +22,12 @@ query {
     currentState
     creator
   }
+}
+`;
+
+const MUTATION_PLACEORDER = gql`
+mutation DeAsx($stockCode:String!, $buyOrSell:String!, $price:Float!, $unit:Int!){
+  placeOrder(stockCode:$stockCode, buyOrSell:$buyOrSell, price:$price, unit:$unit)
 }
 `;
 
@@ -52,7 +60,7 @@ function QueryTransaction() {
     <div>
       <h4 class="heading-3">Recent transactions</h4>
     </div>
-    <ul role="list" class="list">
+    <ul class="list">
       {
         data.queryTransaction.slice(0,8).map(txn => (
           <li class="list-item">
@@ -64,6 +72,68 @@ function QueryTransaction() {
   </div>)
 }
 
+// function PlaceOrderOnClick(){
+//   const [placeOrder, { loading, error, data }] = useMutation(MUTATION_PLACEORDER,
+//     {
+//       variables:{}
+//     });
+
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error :(</p>;
+// }
+
+function PlaceOrderForm(){
+  const [stockCode, setStockCode] = React.useState("");
+  const [buyOrSell, setBuyOrSell] = React.useState("");
+  const [price, setPrice] = React.useState(0.00);
+  const [unit, setUnit] = React.useState(0);
+  const [placeOrder] = useMutation(MUTATION_PLACEORDER);
+  let input = {};
+
+  return (
+    <div class="w-form">
+    <form id="email-form" name="email-form" data-name="Email Form"
+        onSubmit={ e=> {
+          e.preventDefault();
+          console.log(input);
+          placeOrder({variables:{
+            "stockCode": stockCode,
+            "buyOrSell": buyOrSell,
+            "price": price,
+            "unit": unit
+          }});
+
+        }}
+    >
+      <div class="columns-2 w-row">
+        <div class="w-col w-col-6">
+          <label for="StockCode-2" class="uiform-label">Stock Code</label>
+          <select id="StockCode-2" name="StockCode" required="" onChange={e =>setStockCode(e.target.value)} class="select-field w-select">
+            <option value="ANZ">ANZ</option>
+            <option value="CBA">CBA</option>
+          </select>
+          <label for="Price" class="uiform-label">Price</label>
+          <input type="money" class="text-field w-input" maxlength="256" name="Price" onChange={e =>setPrice(parseFloat(e.target.value))} id="Price" required="">            
+          </input>
+        </div>
+        <div class="w-col w-col-6">
+          <label for="BuyOrSell-3" class="uiform-label">Buy or sell</label>
+          <select id="BuyOrSell-3" name="BuyOrSell-3" onChange={e =>setBuyOrSell(e.target.value)}  required="" class="select-field w-select">
+            <option value="Buy">Buy</option>
+            <option value="Sell">Sell</option>
+          </select>
+          <label for="Unit" class="uiform-label">Number of unit</label>
+          <input type="number" class="text-field-2 w-input" maxlength="256" name="Unit" onChange={e =>setUnit(parseInt(e.target.value))}  placeholder="Example Text" id="Unit" required="">
+          </input>
+        </div>
+      </div>
+      <input type="submit" value="Place Order" data-wait="Please wait..." class="submit-button w-button">
+      </input>
+    </form>
+  </div>
+  );
+}
+
 function App() {
   return (
     <ApolloProvider client={client}>
@@ -71,6 +141,7 @@ function App() {
         <header className="App-header">
           <div name="AllanApp">
             <QueryTransaction></QueryTransaction>
+            <PlaceOrderForm></PlaceOrderForm>
           </div>
         </header>
       </div>
